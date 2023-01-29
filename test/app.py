3072e951ec850001e030888aadd1cc5d0e8c84bf
Randark_JMT <randark@foxmail.com>
2023-01-29 00:18:05 +0000

    Update test

---
+++ test/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+import os
+from time import sleep
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    with open("app.py", "r+") as f:
+        return f.read()
+
+
+@app.route("/shell", methods=['POST'])
+def shell():
+    action = request.form["act"]
+    if action.find(" ") != -1:
+        return "Nonono"
+    else:
+        os.system(action)
+    return "Wow"
+
+
+@app.errorhandler(404)
+def error_date(error):
+    sleep(5)
+    return "扫扫扫，扫啥东方明珠呢[怒]"
+
+
+if __name__ == "__main__":
+    app.run()
